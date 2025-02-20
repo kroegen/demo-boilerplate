ac5a8de9644b72b59f5730c769063e9b3b95b94d
Yevhenii Krotenko <kroegen4@gmail.com>
2025-02-20 21:15:37 +0100

    update deps

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ed3775ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v2ka2sfj6r8"
+    "revision": "0.06rsrcg67oo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
